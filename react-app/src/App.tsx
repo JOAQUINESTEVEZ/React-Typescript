74fe7e899c6363aa47b525d0121756f3755e5657
JOAQUINESTEVEZ <joaquin2001estevez@gmail.com>
2024-06-03 15:09:41 -0300

    module styles added

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ["Miami", "New York", "Los Angeles", "San Francisco"];
+  const handleSelectItem = (item: string) => console.log(item);
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClosed={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button
-        color="danger"
-        onClick={() => console.log(setAlertVisibility(true))}
-      >
-        My Button
-      </Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
     </div>
   );
 }
